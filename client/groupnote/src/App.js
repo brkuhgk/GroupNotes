f33ff80837f5d6adb32eeb0ec9dd03415a722681
brkuhgk <krk0241@gmail.com>
2023-07-30 18:50:04 -0400

    added client and server

---
+++ client/groupnote/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
+import TextEditor from './TextEditor';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact >
+          <Redirect to={`/documents/${uuidV4()}`} > </Redirect>
+        </Route>
+
+        <Route path='/documents/:id'>
+          <TextEditor />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
